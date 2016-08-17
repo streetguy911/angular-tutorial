90fe3cd38c640ac33b2eda59ab15e9b87d50e95c
Dmitriy Tielnyi <dmitryt@techfinancials.com>
2016-08-17 15:49:14 +0300

    add view and controller for header

--- app/modules/root/index.js
+++ app/modules/root/index.js
@@ -3,6 +3,8 @@
 import angular from 'angular';
 import rootTemplate from './root.tpl.html';
 import rootController from './rootController';
+import headerTemplate from './content/header.tpl.html';
+import headerController from './content/headerController';
 
 let root = angular.module('app.root', [])
 .config(($stateProvider, $urlRouterProvider, $locationProvider) => {
@@ -14,7 +16,12 @@ let root = angular.module('app.root', [])
         '': {
           template: rootTemplate,
           controller: rootController
-        }
+        },
+          'header': {
+            template: headerTemplate,
+            controller: headerController,
+            controllerAs: 'header'
+          }
       }
     });
 
