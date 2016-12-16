9dfa2ea99523db2ed988c359d6ac4174dff8ac95
Мороз Александр <alieksandr_moroz@mail.ua>
2016-12-16 10:51:35 +0200

    fix appConfig (#10)
    * fix appConfig
    
    * fix appConfig.js according code review

--- app/common/configs/appConfig.js
+++ app/common/configs/appConfig.js
@@ -1,20 +1,19 @@
 'use strict';
 
-let url = 'https://angular-tutorial-server.herokuapp.com';
-//let url = 'http://localhost:5000'
 let config = {
-    api: {
-      product: {
-        list: config.api.url + '/api/product'
-      },
-      category: {
-        list: config.api.url + '/api/category'
-      },
-      auth: {
-        signin: config.api.url + '/auth/signin'
-      }
-    }
-  };
+  api: {
+    url: 'https://angular-tutorial-server.herokuapp.com',
+  }
+};
+config.api.product = {
+  list: config.api.url + '/api/product'
+};
+config.api.category = {
+  list: config.api.url + '/api/category'
+};
+config.api.auth = {
+  signin: config.api.url + '/auth/signin'
+};
 
 
 export default config;
